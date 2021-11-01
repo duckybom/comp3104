dc78b4fec4567cfbd581f4cfa70e399fffe5ef34
Minh Duc Cung <64608402+duckybom@users.noreply.github.com>
2021-11-01 09:44:29 +0700

    package.json changes

---
+++ build/precache-manifest.684459d3d6e6de220c8162875637d6b0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "4869e711042b41eb8a22",
+    "url": "/duckybom/COMP3104/static/css/main.414c38ef.chunk.css"
+  },
+  {
+    "revision": "4869e711042b41eb8a22",
+    "url": "/duckybom/COMP3104/static/js/main.4869e711.chunk.js"
+  },
+  {
+    "revision": "188b38bbfc5595e629ce",
+    "url": "/duckybom/COMP3104/static/js/runtime~main.188b38bb.js"
+  },
+  {
+    "revision": "8ebec04f6c698faf83f8",
+    "url": "/duckybom/COMP3104/static/js/2.8ebec04f.chunk.js"
+  },
+  {
+    "revision": "0dc17d0d2712bcde33671ed0aef2b9da",
+    "url": "/duckybom/COMP3104/index.html"
+  }
+];
